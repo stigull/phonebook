2ef89a7b069a35a320f340a56f2833eb97103ec3
stigull <hertogi@sigma.(none)>
2009-01-04 23:45:51 +0000

    Works

--- views.py
+++ views.py
@@ -9,28 +9,27 @@ from phonebook.forms import UserSearchForm
 from phonebook.db_views import UserSearchView
 
 from user_profile.settings import controller
-from utils import JSONResponse
- 
- 
+from utils.djangoutils import JSONResponse
+
 def search(request):
     context = {}
-    
+
     if request.method == 'GET':
         form = UserSearchForm(data = request.GET)
-        
+
         if form.is_valid():
-            query = form.cleaned_data['query']                         
+            query = form.cleaned_data['query']
             results = UserSearchView.objects.search(query)
-            
+
             context['results'] = results
             context['query'] = query
             nr_of_results = results.count()
-            if request.is_ajax():   
+            if request.is_ajax():
                 jsonobject = {  'has_results': nr_of_results != 0,
                                 'results': render_to_string('phonebook/snippets/search_results_list_min.html', {'results': results, 'user': request.user }),
                                 'nr_of_results': nr_of_results }
-                                
+
                 return JSONResponse(object = jsonobject)
             else:
-                return render_to_response('phonebook/search_results.html', context , context_instance = RequestContext(request))      
+                return render_to_response('phonebook/search_results.html', context , context_instance = RequestContext(request))
     return HttpResponseRedirect(reverse('index'))
