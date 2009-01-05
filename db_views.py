5f999085edc6e40baaab16a2fb3ffbf34ceb4524
johannth <johannth@johannth-laptop.(none)>
2009-01-05 13:45:19 +0000

    Fixed bug that made it impossible to delete users

--- db_views.py
+++ db_views.py
@@ -1,5 +1,5 @@
 #coding: utf-8
- 
+
 from django.db import models
 from django.db.models import Q
 
@@ -8,22 +8,25 @@ from user_profile.settings import controller
 ProfileModel = controller.get_profile_model()
 
 class UserSearchViewManager(models.Manager):
-    
+
     def search(self, query):
         return super(UserSearchViewManager, self).get_query_set().filter(
                                     Q(username__icontains = query) |
-                                    Q(fullname__icontains = query) | 
-                                    Q(fullname_sans_middlename__icontains = query) | 
-                                    Q(first_name__icontains = query) | 
+                                    Q(fullname__icontains = query) |
+                                    Q(fullname_sans_middlename__icontains = query) |
+                                    Q(first_name__icontains = query) |
                                     Q(middlenames__icontains = query) |
-                                    Q(last_name__icontains = query) | 
+                                    Q(last_name__icontains = query) |
                                     Q(address__icontains = query) |
                                     Q(phone__icontains = query) |
                                     Q(gsm__icontains = query)).select_related().order_by("fullname")[:5]
 
 
 class UserSearchView(models.Model):
-    profile = models.ForeignKey(ProfileModel, primary_key = True)
+    """
+    Note to self: ForeignKeys to not work when using database views
+    """
+    profile_id = models.IntegerField(primary_key = True)
     username = models.CharField(max_length = 30)
     kennitala = models.CharField(max_length = 11)
     fullname = models.CharField(max_length = 600)
@@ -34,11 +37,16 @@ class UserSearchView(models.Model):
     address = models.CharField(max_length = 255)
     phone = models.CharField(max_length = 7)
     gsm = models.CharField(max_length = 7)
-    
+
     objects = UserSearchViewManager()
-    
+
+    def get_profile(self):
+        return ProfileModel.objects.get(id = self.profile_id)
+    profile = property(get_profile)
+
+
     class Meta:
         db_table = "phonebook_usersearchview"
 
-    
-    
+
+
--- management.py
+++ management.py
@@ -1,51 +1,51 @@
 #coding: utf-8
 from django.db import connection, backend, models
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 
 import user_profile
 from user_profile.settings import controller
 
+PROFILE_MODEL = controller.get_profile_model()
 
 def create_search_view(sender, app, created_models, verbosity, interactive, **kwargs):
-    profile_model = controller.get_profile_model()
     VIEW_SQL = """
                 CREATE OR REPLACE VIEW phonebook_usersearchview
-                    (profile_id, 
-                        username, 
-                        kennitala, 
+                    (profile_id,
+                        username,
+                        kennitala,
                         fullname,
                         fullname_sans_middlename,
                         first_name,
                         middlenames,
                         last_name,
-                        address, 
-                        phone, 
+                        address,
+                        phone,
                         gsm)
                 AS
-                    SELECT 
-                        p.id as profile_id, 
-                        u.username as username, 
-                        p.kennitala as kennitala, 
-                        u.first_name || ' ' || p.middlenames || ' ' || u.last_name as fullname, 
+                    SELECT
+                        p.id as profile_id,
+                        u.username as username,
+                        p.kennitala as kennitala,
+                        u.first_name || ' ' || p.middlenames || ' ' || u.last_name as fullname,
                         u.first_name || ' ' || u.last_name as fullname_sans_middlename,
                         u.first_name as first_name,
                         p.middlenames as middlenames,
                         u.last_name as last_name,
-                        p.address as address, 
-                        p.phone as phone, 
+                        p.address as address,
+                        p.phone as phone,
                         p.gsm as gsm
-                    FROM 
-                        %(user_table_name)s as u, 
+                    FROM
+                        %(user_table_name)s as u,
                         %(profile_table_name)s as p
-                    WHERE 
+                    WHERE
                         u.id = p.user_id
                     ORDER BY fullname
-                """ % {'user_table_name': User._meta.db_table, 
-                        'profile_table_name': profile_model._meta.db_table }
-                
+                """ % {'user_table_name': User._meta.db_table,
+                        'profile_table_name': PROFILE_MODEL._meta.db_table }
+
     cursor = connection.cursor()
     cursor.execute(VIEW_SQL)
+    cursor.close()
+
 
 models.signals.post_syncdb.connect(create_search_view, sender = user_profile.models)
-    
